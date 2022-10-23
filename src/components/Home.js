77f45bbde4ef853d83983f162480bd103bab7594
huynhquythanh113 <72460391+huynhquythanh113@users.noreply.github.com>
2022-10-23 17:36:21 +0700

    code todo

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+export const HomePage = () => {
+  return (
+    <>
+      <h2>MY HOMEPAGE</h2>
+      <div>Mã số sinh viên : 4501104212</div>
+      <div>Họ và tên : Huỳnh Quý Thanh </div>
+      <div>Email : 4501104212@student.hcmue.edu.vn </div>
+    </>
+  );
+};
